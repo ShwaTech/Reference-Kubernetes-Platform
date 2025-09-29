d7ade2e037681ab6f57881ad5cb5cc3501c51c5c
ShwaTech <shwakesh13579@gmail.com>
2025-09-29 11:53:08 +0300

    Foundations Done

--- 00_Foundations/main.tf
+++ 00_Foundations/main.tf
@@ -107,7 +107,7 @@ module "cert_manager_irsa_role" {
 }
 
 
-## 
+## Create IAM roles for Kubernetes service accounts with specific permissions
 module "external_secrets_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.32.0"
